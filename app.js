85cee162c695ebd36b1a55d7e92d04622235625d
G Schulz <97812180+georgeschulz@users.noreply.github.com>
2022-08-04 21:09:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch'); // import node-fetch (enables the fetch API
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
-  es.send(`<h1>Today's Activity: Hello world!</h1>`)
+  res.send(`<h1>Today's Activity: Hello world!</h1>`)
 })
 
 app.listen(PORT, () => { // start server and listen on specified port
